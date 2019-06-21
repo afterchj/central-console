6b8a0aafdbf4b526ce8d99a153412399f45b9d69
李楠楠 <nannan.li@tpadsz.cn>
2019-06-21 13:37:17 +0800

    。。。。

--- src/main/java/com/example/blt/dao/LightListDao.java
+++ src/main/java/com/example/blt/dao/LightListDao.java
@@ -28,7 +28,7 @@ public interface LightListDao {
     @Select("SELECT d.lmac,d.mname,d.lname  ,CASE WHEN i.x = '32' AND i.y = '32' THEN '1' WHEN i.x is not NULL AND ( i.x != '32' OR i.y != '32' ) THEN '0' ELSE NULL END AS STATUS,d.Group FROM f_light_demo d LEFT JOIN (select  lmac,x ,y from t_light_info Group by lmac) i ON d.lmac = i.lmac ORDER BY d.mname,lname")//1:关 0：开 null 空
     List<LightDemo> getLightInfo();
 
-    @Select("SELECT d.lmac ,d.mname,d.lname ,CASE WHEN i.y = '32' THEN '1' when i.y is null and i.status ='0' then '1' when i.y is null and i.status ='1' then '0' WHEN i.y != '32' and i.y!=null THEN '0' when i.y is null and i.status is null then null  END AS status,d.Group  FROM f_light_demo d LEFT JOIN (select  lmac,x ,y,status from t_light_info Group by lmac) i ON d.lmac = i.lmac where d.mname='Office' ORDER BY d.mname,lname")
+    @Select("SELECT d.lmac ,d.mname,d.lname ,CASE WHEN i.y = '32' THEN '1' WHEN i.y != '32' and i.y is not null THEN '0' when i.y is null and i.status ='0' then '1' when i.y is null and i.status ='1' then '0'  when i.y is null and i.status is null then null  END AS status,d.Group  FROM f_light_demo d LEFT JOIN (select  lmac,x ,y,status from t_light_info Group by lmac) i ON d.lmac = i.lmac where d.mname='Office' ORDER BY d.mname,lname")
     List<LightDemo> getOfficeLightInfo();
 
     @Select("select count(*) as PlaceLNum,d.mname,d.place from f_light_demo d,t_light_info i where d.lmac=i.lmac and (i.y is not null or i.status is not null) and d.mname='Office' Group by mname,place;")
