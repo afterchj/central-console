5c94d8f9db76abee95ecb42180428935328b442c
chenhao.lu <chenhao.lu>
2019-07-12 17:58:41 +0800

    。。。

--- src/main/java/com/example/blt/controller/MonitorController.java
+++ src/main/java/com/example/blt/controller/MonitorController.java
@@ -181,11 +181,12 @@ public class MonitorController {
                 } else {
                     status = "1";
                 }
+                int place = monitor4Dao.getPlace(lightDemo.getMname(), groupId);
                 lightDemo.setMname(getMname(commandInfo.getHost()));
                 lightDemo.setGroupId(groupId);
                 lightDemo.setStatus(status);
                 lightDemo.setOther("group");
-                int place = monitor4Dao.getPlace(lightDemo.getMname(), groupId);
+                lightDemo.setPlace(place);
                 List<Integer> statusList1 = monitor4Dao.getStatusOfPlace(lightDemo.getMname(), place, groupId);
                 List<Integer> statusList2 = monitor4Dao.getStatusOfFloor(lightDemo.getMname(), place, groupId);
                 Map map1 = getExceptionAndDiff(statusList1);
@@ -207,7 +208,6 @@ public class MonitorController {
                 }
                 map.put("placeStatus", placeStatus);
                 map.put("floorStatus", floorStatus);
-                map.put("place", place);
                 map.put("placeException", map1.get("exception"));
                 map.put("placeDifference", map1.get("difference"));
                 map.put("floorException", map2.get("exception"));
