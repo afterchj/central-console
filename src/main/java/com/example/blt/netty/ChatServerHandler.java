c27f2884441ef11e92edca82abb8be3234a2db3c
hongjian.chen <766256898@qq.com>
2019-07-16 14:55:37 +0800

    优化代码

--- src/main/java/com/example/blt/netty/ChatServerHandler.java
+++ src/main/java/com/example/blt/netty/ChatServerHandler.java
@@ -87,20 +87,19 @@ public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) {
         Channel channel = ctx.channel();
-        insertOrUpdateHost(ctx);
         group.add(channel);
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) {
         Channel channel = ctx.channel();
-        insertOrUpdateHost(ctx);
         group.remove(channel);
     }
 
     //在建立链接时发送信息
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
+        insertOrUpdateHost(ctx);
     }
 
     //退出链接
@@ -109,8 +108,13 @@ public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        ctx.close().sync();
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        try {
+            ctx.close().sync();
+        } catch (InterruptedException e) {
+        } finally {
+            insertOrUpdateHost(ctx);
+        }
     }
 
     private void insertOrUpdateHost(ChannelHandlerContext ctx) {
