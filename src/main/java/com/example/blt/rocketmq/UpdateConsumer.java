0ce70018958f75fc499cfae9d494c6ec1da0b76f
hongjian.chen <766256898@qq.com>
2019-07-16 14:38:00 +0800

    新增更新灯信息TOPIC

---
+++ src/main/java/com/example/blt/rocketmq/UpdateConsumer.java
@@ -0,0 +1,39 @@
+package com.example.blt.rocketmq;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+
+/**
+ * @author hongjian.chen
+ * @date 2019/6/25 16:26
+ */
+
+@Slf4j
+@Service
+@RocketMQMessageListener(topic = "update_light_topic", consumerGroup = "remote_light_consumer_update_group")
+public class UpdateConsumer implements RocketMQListener<String> {
+
+    @Resource
+    private SqlSessionTemplate sqlSessionTemplate;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void onMessage(String message) {
+        try {
+            Map map = JSON.parseObject(message);
+            sqlSessionTemplate.selectOne("console.saveUpdate2", map);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
