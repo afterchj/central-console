8b092ff3154952453497614827879121ea200489
李楠楠 <nannan.li@tpadsz.cn>
2019-09-03 13:43:26 +0800

    点击登录controller

--- src/main/java/com/example/blt/controller/ControlCenterController.java
+++ src/main/java/com/example/blt/controller/ControlCenterController.java
@@ -16,41 +16,60 @@ public class ControlCenterController {
 
     /**
      * 跳转到login.html
-     * @param model
+     *
      * @return
      */
     @RequestMapping("/login")
-    public String login(Model model){
+    public String login() {
         return "poeConsole/login";
     }
 
     /**
+     * 点击登录 跳转到index.html
+     * @param model
+     * @param username
+     * @param password
+     * @return
+     */
+    @RequestMapping("/toLogin")
+    public String toLogin(Model model, String username, String password) {
+//        if (username.equals("admin") && password.equals("admin")){
+//            return "poeConsole/index";
+//        }else {
+        return "poeConsole/index";
+//        }
+    }
+
+    /**
      * 跳转到index.html
+     *
      * @param model
      * @return
      */
     @RequestMapping("/index")
-    public String index(Model model){
+    public String index(Model model) {
         return "poeConsole/index";
     }
 
     /**
      * 跳转到timing.html
+     *
      * @param model
      * @return
      */
     @RequestMapping("/timer")
-    public String timer(Model model){
+    public String timer(Model model) {
         return "poeConsole/timing";
     }
 
     /**
      * 跳转到device.html
+     *
      * @param model
      * @return
      */
     @RequestMapping("/netWorkGroupConsole")
-    public String netWorkGroupConsole(Model model){
+    public String netWorkGroupConsole(Model model) {
         return "poeConsole/device";
     }
 }
