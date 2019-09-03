214dc86522e20920ad9e5c3e3e35f9b7bdc3ac40
李楠楠 <nannan.li@tpadsz.cn>
2019-09-03 13:32:01 +0800

    群控中心controller

---
+++ src/main/java/com/example/blt/controller/ControlCenterController.java
@@ -0,0 +1,56 @@
+package com.example.blt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: central-console
+ * @description: 定时功能
+ * @author: Mr.Ma
+ * @create: 2019-09-03 13:13
+ **/
+@Controller
+@RequestMapping("/control")
+public class ControlCenterController {
+
+    /**
+     * 跳转到login.html
+     * @param model
+     * @return
+     */
+    @RequestMapping("/login")
+    public String login(Model model){
+        return "poeConsole/login";
+    }
+
+    /**
+     * 跳转到index.html
+     * @param model
+     * @return
+     */
+    @RequestMapping("/index")
+    public String index(Model model){
+        return "poeConsole/index";
+    }
+
+    /**
+     * 跳转到timing.html
+     * @param model
+     * @return
+     */
+    @RequestMapping("/timer")
+    public String timer(Model model){
+        return "poeConsole/timing";
+    }
+
+    /**
+     * 跳转到device.html
+     * @param model
+     * @return
+     */
+    @RequestMapping("/netWorkGroupConsole")
+    public String netWorkGroupConsole(Model model){
+        return "poeConsole/device";
+    }
+}
