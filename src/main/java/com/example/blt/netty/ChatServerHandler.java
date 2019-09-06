b28b43cc84accc2c39954d0a59ff7839a2f2e205
hongjian.chen <766256898@qq.com>
2019-09-06 18:32:57 +0800

    。。。

--- src/main/java/com/example/blt/netty/ChatServerHandler.java
+++ src/main/java/com/example/blt/netty/ChatServerHandler.java
@@ -68,10 +68,13 @@ public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
             if (arg1.indexOf("77050304") != -1) {
                 cmd = "77050103";
             }
-            if (len >= 48 && len <= 52) {
-                if (arg1.indexOf("77050705") != -1) {
-                    cmd = "77050103";
+            if (arg1.indexOf("77050705") != -1) {
+                cmd = "77050103";
+                if (len >= 48 && len <= 52) {
                     String mac = StringBuildUtils.sortMac(arg1.substring(36, 48));
+                    insertOrUpdateHost(channel, "", mac);
+                } else {
+                    String mac = StringBuildUtils.sortMac(arg1.substring(8, 20));
                     insertOrUpdateHost(channel, "", mac);
                 }
             }
