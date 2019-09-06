e1c9c88ba8f8cfbcb682f71f49ad71611f7fb08e
hongjian.chen <766256898@qq.com>
2019-09-06 18:13:02 +0800

    新增选择开关功能

--- src/main/java/com/example/blt/WebMvcConfig.java
+++ src/main/java/com/example/blt/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.example.blt.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -13,10 +14,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/index").setViewName("index");
-//    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/old").setViewName("oldIndex");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
