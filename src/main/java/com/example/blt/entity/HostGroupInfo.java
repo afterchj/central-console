d835c136ec4833afe1fd309037ab5b6b03e54092
hongjian.chen <766256898@qq.com>
2019-09-24 17:42:47 +0800

    新增t_host_group表结构

--- src/main/java/com/example/blt/entity/HostGroupInfo.java
+++ src/main/java/com/example/blt/entity/HostGroupInfo.java
@@ -9,16 +9,12 @@ import java.util.Date;
  */
 
 @Entity
-@Table(name = "t_group")
-public class GroupInfo implements Serializable {
+@Table(name = "t_host_group")
+public class HostGroupInfo implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(length = 32)
-    private String gname;
-    private Integer groupId;
-    private String other;
-    private Date createDate = new Date();
-    private Date updateDate = new Date();
-
+    private Integer gid;
+    private Integer hid;
 }
