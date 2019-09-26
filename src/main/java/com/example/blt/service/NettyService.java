46b298eaa975344f7cfb1c5d34b3468233460312
hongjian.chen <766256898@qq.com>
2019-09-26 15:40:25 +0800

    。。。

--- src/main/java/com/example/blt/service/NettyService.java
+++ src/main/java/com/example/blt/service/NettyService.java
@@ -47,7 +47,7 @@ public class NettyService implements ApplicationListener<ContextRefreshedEvent> 
                     list2.add(host);
                 }
             }
-            saveHostStatus(list1, list2);
+//            saveHostStatus(list1, list2);
         } catch (Exception e) {
             logger.error("updateHostStatus error {}", e.getMessage());
         }
@@ -125,12 +125,11 @@ public class NettyService implements ApplicationListener<ContextRefreshedEvent> 
         logger.warn("online hosts {}", list2);
         if (list2.size() > 0) {
             sqlSessionTemplate.update("console.flushHostsStatus", list2);
+        }else {
+            sqlSessionTemplate.update("console.saveHostsStatus", list2);
         }
         if (list2.size() > 0 && list2.size() != list1.size()) {
             sqlSessionTemplate.update("console.updateHostsStatus", list2);
-        }
-        if (list2.size() == 0) {
-            sqlSessionTemplate.update("console.saveHostsStatus", list2);
         }
     }
 
