2559a8f6c7f0026b0faa378323b5f66ca48f9876
hongjian.chen <766256898@qq.com>
2019-09-26 10:00:23 +0800

    更新t_host_info表

--- src/main/java/com/example/blt/entity/HostInfo.java
+++ src/main/java/com/example/blt/entity/HostInfo.java
@@ -17,147 +17,21 @@ public class HostInfo implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String ip;
-    private Integer gid;
     @Column(unique = true, length = 32)
     private String hostId;
-    @Column(length = 32)
-    private String meshId;
-    @Column(length = 32)
-    private String meshName;
-    @ColumnDefault("1")
-    private Boolean status;
-    @ColumnDefault("0")
-    private Boolean isMaster;
-    @ColumnDefault("1")
-    private Boolean isControl;
     @Column(unique = true, length = 32)
     private String mac;
     @Column(length = 32)
     private String productType;
     @Column(length = 32)
     private String otaVersion;
-    @ColumnDefault("03")
-    private String flag;
+    @ColumnDefault("1")
+    private Boolean status;
+    @ColumnDefault("0")
+    private Boolean isMaster;
+    @ColumnDefault("1")
+    private Boolean isControl;
     private String other;
     private Date log_date = new Date();
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public Integer getGid() {
-        return gid;
-    }
-
-    public void setGid(Integer gid) {
-        this.gid = gid;
-    }
-
-    public String getHostId() {
-        return hostId;
-    }
-
-    public void setHostId(String hostId) {
-        this.hostId = hostId;
-    }
-
-    public String getMeshId() {
-        return meshId;
-    }
-
-    public void setMeshId(String meshId) {
-        this.meshId = meshId;
-    }
-
-    public String getMeshName() {
-        return meshName;
-    }
-
-    public void setMeshName(String meshName) {
-        this.meshName = meshName;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-
-    public Boolean getMaster() {
-        return isMaster;
-    }
-
-    public void setMaster(Boolean master) {
-        isMaster = master;
-    }
-
-    public Boolean getControl() {
-        return isControl;
-    }
-
-    public void setControl(Boolean control) {
-        isControl = control;
-    }
-
-    public String getMac() {
-        return mac;
-    }
-
-    public void setMac(String mac) {
-        this.mac = mac;
-    }
-
-    public String getProductType() {
-        return productType;
-    }
-
-    public void setProductType(String productType) {
-        this.productType = productType;
-    }
-
-    public String getOtaVersion() {
-        return otaVersion;
-    }
-
-    public void setOtaVersion(String otaVersion) {
-        this.otaVersion = otaVersion;
-    }
-
-    public String getFlag() {
-        return flag;
-    }
-
-    public void setFlag(String flag) {
-        this.flag = flag;
-    }
-
-    public String getOther() {
-        return other;
-    }
-
-    public void setOther(String other) {
-        this.other = other;
-    }
-
-    public Date getLog_date() {
-        return log_date;
-    }
-
-    public void setLog_date(Date log_date) {
-        this.log_date = log_date;
-    }
 }
