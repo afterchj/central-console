008ee81f1b7dfb6fb3207ad86e3c9c9761ca5ce5
hongjian.chen <766256898@qq.com>
2019-11-08 17:03:12 +0800

    群控分组表结构设计

--- src/main/java/com/example/blt/entity/MasterGroup.java
+++ src/main/java/com/example/blt/entity/MasterGroup.java
@@ -2,19 +2,17 @@ package com.example.blt.entity;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * Created by hongjian.chen on 2019/5/31.
  */
 
 @Entity
-@Table(name = "t_mesh_group")
-public class MeshGroup implements Serializable {
+@Table(name = "t_master_group")
+public class MasterGroup implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private Integer gid;
-    private Integer mid;
-    private Integer masterId;
+    private Integer childId;
+    private Integer hid;
 }
